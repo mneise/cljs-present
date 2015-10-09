6c722bade33a3eadcd77c586a554d5e7cca0c5a1
Maria Geller <maria.geller2@gmail.com>
2015-10-09 22:31:23 +1300

    Change slide on swipe right and left.

--- src/cljs_present/core.cljs
+++ src/cljs_present/core.cljs
@@ -2,47 +2,67 @@
   (:require [goog.events :as events]
             [goog.dom :as dom]
             [clojure.string :as str]
-            [cljs-present.code :as code])
+            [cljs-present.code :as code]
+            [hammer :as hammer])
   (:import [goog.events KeyHandler]))
 
 (enable-console-print!)
 
 (def idx (atom 0))
 
-(defn next-slide [slide-count]
+(defn change-slide [slide-count direction]
+  (let [active (.-activeElement js/document)
+        slide (dom/getElement (str "slide-" @idx))
+        input (dom/getElementByClass "CodeMirror" slide)]
+    (when (or (not (and active input))
+              (not (dom/contains input active)))
+      (let [diff (case direction
+                   :left -1
+                   :right 1
+                   0)
+            next-idx (mod (+ @idx diff) slide-count)
+            next (dom/getElement (str "slide-" next-idx))]
+        (dom/setProperties slide #js {:class "hidden slide"})
+        (dom/setProperties next #js {:class "current slide"})
+        (reset! idx next-idx)))))
+
+(defn keydown [slide-count]
   (fn [e]
-    (let [active (.-activeElement js/document)
-          slide (dom/getElement (str "slide-" @idx))
-          input (dom/getElementByClass "CodeMirror" slide)]
-      (when (or (not (and active input))
-                (not (dom/contains input active)))
-        (let [diff (case (.-keyCode e)
-                     37 -1
-                     39 1
-                     0)
-              next-idx (mod (+ @idx diff) slide-count)
-              next (dom/getElement (str "slide-" next-idx))]
-          (dom/setProperties slide #js {:class "hidden slide"})
-          (dom/setProperties next #js {:class "current slide"})
-          (reset! idx next-idx))))))
+    (let [direction (case (.-keyCode e)
+                      37 :left
+                      39 :right
+                      0)]
+      (change-slide slide-count direction))))
+
+(defn swiperight [slide-count]
+  (fn [e]
+    (change-slide slide-count :right)))
+
+(defn swipeleft [slide-count]
+  (fn [e]
+    (change-slide slide-count :left)))
 
 (defn hide-all-slides [count]
   (doseq [i (range count)]
     (let [slide (dom/getElement (str "slide-" i))]
       (dom/setProperties slide #js {:class "hidden slide"}))))
 
-(defn setup-listener [slide-count]
-  (let [doc-kh (KeyHandler. js/document)]
-    (events/listen doc-kh "key" (next-slide slide-count))))
+(defn setup-listeners [slide-count]
+  (let [doc-kh (KeyHandler. js/document)
+        h (js/hammer. (dom/getElement "slides"))]
+    (events/listen doc-kh "key" (keydown slide-count))
+    (.on h "swiperight" (swiperight slide-count))
+    (.on h "swipeleft" (swipeleft slide-count))))
 
 (defn load-slides []
   (let [slides (dom/getElementsByClass "slide")
-        keys (.keys js/Object slides)]
+        keys (.keys js/Object slides)
+        slide-count (count keys)]
     (doseq [k keys]
       (dom/setProperties (aget slides k) #js {:id (str "slide-" k)}))
-    (code/setup (count keys))
-    (hide-all-slides (count keys))
-    (setup-listener (count keys))
+    (code/setup slide-count)
+    (hide-all-slides slide-count)
+    (setup-listeners slide-count)
     (dom/setProperties (dom/getElement "slide-0") #js {:class "current slide"})))
 
 (load-slides)
